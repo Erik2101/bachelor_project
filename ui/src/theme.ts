54645fa40a93d0d5fca2c24df4f69289484c0543
Erik Semmler <erik.semmler@stud.htwk-leipzig.de>
2022-05-30 08:38:56 +0200

    init theme

---
+++ ui/src/theme.ts
@@ -0,0 +1,18 @@
+const palette = {
+    primary_grey : "",
+    secondary_grey : "", 
+    tertiary_grey : "",
+    black : "",
+    primary_green : "#025939",
+    secondary_green : "#027333",
+    tertiary_green : "#03A63C",
+    highlight_green : "#04D939",
+    primary_blue : "#012E40",
+    secondary_blue : "#024959",
+    tertiary_blue : "#026773",
+    highlight_blue : "#3CA6A6",
+}
+
+export const theme = {
+
+}
